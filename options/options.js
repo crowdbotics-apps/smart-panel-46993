36a46f2af4bd196eb96ba5469e9066e17dbc65f8
Crowdbotics <team@crowdbotics.com>
2024-03-13 11:41:48 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "smart-panel-46993",
+  url: "https://smart-panel-46993.botics.co",
+  api: "https://smart-panel-46993.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
